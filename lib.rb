ca597ffda4a5857dd3cd7721254f69fadef38ede
jasonblanchard <jas.blanchard@gmail.com>
2013-03-27 11:56:01 -0400

    Changing them all to one date

--- lib.rb
+++ lib.rb
@@ -1,4 +1,4 @@
-$TERM_DATES = { 1 => "04/01/2013" }
+$TERM_DATES = { 1 => "2013-4-02 23:59:00" }
 
 class Course
 
@@ -23,22 +23,25 @@ class Course
 
     def sidebar_links
         @mechanize.get(@entry_point) do |page|
-            @links = page.links_with(:text => /[week|unit] \d$/i)
+            @links = page.links_with(:text => /[week|unit] \d+$/i)
         end
 
-        content_id = /content_id=_(\d+)/.match(@links[0].href)[1]
-        form_url = "https://learn.dcollege.net/webapps/blackboard/content/manageFolder.jsp?content_id=_#{content_id}_1&course_id=_#{@course_id}_1"
+        @links.each do |week_link|
 
-        @mechanize.get(form_url) do |form_page|
+            content_id = /content_id=_(\d+)/.match(week_link.href)[1]
+            form_url = "https://learn.dcollege.net/webapps/blackboard/content/manageFolder.jsp?content_id=_#{content_id}_1&course_id=_#{@course_id}_1"
+            week_number = /[week|unit] (\d+)/.match(week_link.text)[1]
 
-            form = form_page.form_with(:name => 'the_form')
+            @mechanize.get(form_url) do |form_page|
 
-            form_page.form_with(:name => 'the_form') do |f|
-                f.checkbox_with(:name => "bbDateTimePicker_start_checkbox").value = 1
-                f.checkbox_with(:name => "bbDateTimePicker_start_checkbox").check
-                f.bbDateTimePicker_start_datetime = "2013-4-01 23:59:00"
-                f.submit(f.buttons[1])
+                form_page.form_with(:name => 'the_form') do |f|
+                    f.checkbox_with(:name => "bbDateTimePicker_start_checkbox").value = 1
+                    f.checkbox_with(:name => "bbDateTimePicker_start_checkbox").check
+                    f.bbDateTimePicker_start_datetime = $TERM_DATES[1]
+                    f.submit(f.buttons[1])
+                end
             end
+            puts "#{@course_id} - changed date for week #{week_number} to #{$TERM_DATES[1]}"
         end
 
     end
